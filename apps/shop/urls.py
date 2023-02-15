32dbbac50d5e61538a758244eb0d5ca9d9a816e0
r-elen <elen.ryazanova@gmail.com>
2023-02-15 15:50:32 +0300

    images load on wishlist page

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import ViewCart, ViewProductSingle, ViewWishlist
+from .views import ViewCart, ViewProductSingle
 
 app_name = 'shop'
 
 urlpatterns = [
    path('', ViewCart.as_view(), name='shop'),
-   path('', ViewProductSingle.as_view(), name='product-single'),
-   path('', ViewWishlist.as_view(), name='wishlist'),
+   path('product-single/', ViewProductSingle.as_view(), name='product-single'),
 
 ]
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -13,7 +13,3 @@ class ViewCart(View):
 class ViewProductSingle(View):
    def get(self, request):
        return render(request, 'shop/product-single.html')
-
-class ViewWishlist(View):
-   def get(self, request):
-       return render(request, 'shop/wishlist.html')
