287d2b9e0fe67b7d81854e5b31e8328de1dd70ad
r-elen <elen.ryazanova@gmail.com>
2023-02-18 17:44:15 +0300

    try to add delete, add, and checkout

--- apps/cart_shop/urls.py
+++ apps/cart_shop/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import ViewCart, ViewWishlist
+from .views import ViewCart, ViewWishlist, ViewCartBuy, remove_item_wishlist, update_item_wishlist, checkout_wishlist
 
 app_name = 'cart_shop'
 
 urlpatterns = [
    path('', ViewCart.as_view(), name='cart'),
+   path('buy/<int:product_id>', ViewCartBuy.as_view(), name='buy'),
    path('wishlist/', ViewWishlist.as_view(), name='wishlist'),
+   path('wishlist/update_item/<int:item_id>/', update_item_wishlist, name='update_item'),
+   path('wishlist/remove_item/<int:item_id>/', remove_item_wishlist, name='remove_item'),
+   path('wishlist/checkout/', checkout_wishlist, name='checkout'),
 ]
--- apps/cart_shop/views.py
+++ apps/cart_shop/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
 from django.views import View
-from .models import Wishlist, CartItemsNew
+from .models import Wishlist, CartItemsNew, SingleProduct, CartUser
 
 
 class ViewCart(View):
@@ -14,6 +15,39 @@ class ViewCart(View):
         return render(request, 'cart_shop/cart.html', context)
 
 
+class ViewCartBuy(View):
+   def get(self, request, product_id):
+       product = get_object_or_404(SingleProduct, id=product_id)
+       cart_user = get_object_or_404(CartUser, user=request.user)
+       cart_item = CartItemsNew(cart=cart_user, product=product)
+       cart_item.save()
+
+
+def view_cart_total(request):
+   cart_items = Wishlist.objects.filter(cart__user=request.user)
+   total_price = sum(item.product.price * item.quantity for item in cart_items)
+   context = {'cart_items': cart_items, 'total_price': total_price}
+   return render(request, 'cart_shop/wishlist.html', context)
+
+
+def update_item(request, item_id):
+   item = Wishlist.objects.get(id=item_id)
+   item.quantity += int(request.GET.get('quantity'))
+   item.save()
+   return redirect('cart_shop:view_cart_wishlist')
+
+
+def del_from_cart(request, item_id):
+   Wishlist.objects.filter(id=item_id).delete()
+   return redirect('cart_shop:view_cart_wishlist')
+
+
+def checkout_wishlist(request):
+   # code to handle checkout process
+   return redirect('cart_shop:view_cart_wishlist')
+
+
+
 class ViewWishlist(View):
    def get(self, request):
        data = Wishlist.objects.all()
@@ -21,3 +55,25 @@ class ViewWishlist(View):
 
        return render(request, 'cart_shop/wishlist.html', context)
 
+def view_cart_wishlist(request):
+   cart_items = Wishlist.objects.filter(cart__user=request.user)
+   total_price = sum(item.product.price * item.quantity for item in cart_items)
+   context = {'cart_items': cart_items, 'total_price': total_price}
+   return render(request, 'cart_shop/wishlist.html', context)
+
+
+def update_item_wishlist(request, item_id):
+   item = Wishlist.objects.get(id=item_id)
+   item.quantity += int(request.GET.get('quantity'))
+   item.save()
+   return redirect('cart_shop:view_cart_wishlist')
+
+
+def remove_item_wishlist(request, item_id):
+   Wishlist.objects.filter(id=item_id).delete()
+   return redirect('cart_shop:view_cart_wishlist')
+
+
+def checkout_wishlist(request):
+   # code to handle checkout process
+   return redirect('cart_shop:view_cart_wishlist')
