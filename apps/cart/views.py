7e1cc9bec30e3e105c0751d610d6f02a026ba574
r-elen <elen.ryazanova@gmail.com>
2023-02-18 16:11:52 +0300

    no data from base in wishlist page

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect
-from .models import Cart, CartItem
+from .models import Cart, CartItem, Product
 from django.views import View
+
 
 class CartView(View):
    def get(self, request):
-       # cart_items = Cart.Item.objects.filter(cart__user=request.user)
-       # total_price = sum(item.product.price * item.quantity for )
-       # context = {'cart_items': cart_item, 'total'}
-       # return render(request, 'cart.html', context)
-       pass
+      data = Product.objects.all()
+      context = {'data': data}
+
+      return render(request, 'cart/cart.html', context)
+
 
 def view_cart(request):
    cart_items = CartItem.objects.filter(cart__user=request.user)
--- apps/cart_shop/views.py
+++ apps/cart_shop/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.views import View
+from .models import CartUser, SingleProduct, Wishlist
 
 
 class ViewCart(View):
@@ -11,34 +12,8 @@ class ViewCart(View):
 
 class ViewWishlist(View):
    def get(self, request):
-       context = {'data': [
-                            {'name': 'Bell Pepper',
-                            'price': 4.90,
-                            'total_price': 4.90,
-                            'url': 'shop/images/product-1.jpg'},
-
-                            {'name': 'Strawberry',
-                            'price': 15.70,
-                            'total_price': 15.70,
-                            'url': 'shop/images/product-2.jpg'},
-
-                            {'name': 'Green Beans',
-                            'price': 25.90,
-                            'total_price': 25.90,
-                            'url': 'shop/images/product-3.jpg'},
-
-                            {'name': 'Purple Cabbage',
-                            'price': 7.50,
-                            'total_price': 7.50,
-                            'url': 'shop/images/product-4.jpg'},
-
-                            {'name': 'Tomatoe',
-                            'price': 12.40,
-                            'total_price': 12.40,
-                            'url': 'shop/images/product-5.jpg'},
-
-                           ]
-                  }
+       data = Wishlist.objects.all()
+       context = {'data': data}
 
        return render(request, 'cart_shop/wishlist.html', context)
 
