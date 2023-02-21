f946ea478596747bb7810612265d197945099a21
r-elen <elen.ryazanova@gmail.com>
2023-02-21 14:38:51 +0300

    reload BD
    main page not working

--- apps/cart_shop/urls.py
+++ apps/cart_shop/urls.py
@@ -5,8 +5,8 @@ app_name = 'cart_shop'
 
 urlpatterns = [
    path('', ViewCart.as_view(), name='cart'),
-   path('add/<int:product_id>', ViewCartAdd.as_view(), name='add_to_cart'),
-   path('buy/<int:product_id>', ViewCartBuy.as_view(), name='buy'),
+   path('add/<int:item_id>', ViewCartAdd.as_view(), name='add_to_cart'),
+   path('buy/<int:item_id>', ViewCartBuy.as_view(), name='buy'),
    path('del/<int:item_id>', ViewCartDel.as_view(), name='del_from_cart'),
 
    path('wishlist/', ViewWishlist.as_view(), name='wishlist'),
