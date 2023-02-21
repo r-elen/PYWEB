b37ed72715f251a4f271f0afa59f827e76aebb86
r-elen <elen.ryazanova@gmail.com>
2023-02-21 16:14:40 +0300

    image = models.ImageField(upload_to='static/products/', null=True, blank=True, default='static/products/')

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
    name = models.CharField(max_length=255)
    price = models.DecimalField(max_digits=10, decimal_places=2)
    description = models.TextField()
-   image = models.ImageField(upload_to='products/', null=True, blank=True, default='products/')
+   image = models.ImageField(upload_to='static/products/', null=True, blank=True, default='static/products/')
    created_at = models.DateTimeField(auto_now_add=True)
    updated_at = models.DateTimeField(auto_now=True)
 
