0bb0d488908b26237eba3eb436030deda8012616
r-elen <elen.ryazanova@gmail.com>
2023-02-21 21:11:48 +0300

    rework discount and price in main page (home/index.html)

--- apps/cart_shop/models.py
+++ apps/cart_shop/models.py
@@ -16,6 +16,8 @@ class SingleProduct(models.Model):
    price = models.DecimalField(max_digits=10, decimal_places=2)
    description = models.TextField()
    image = models.ImageField(upload_to='static/products/', null=True, blank=True)
+   discount = models.IntegerField(null=True, blank=True)
+
    created_at = models.DateTimeField(auto_now_add=True)
    updated_at = models.DateTimeField(auto_now=True)
 
